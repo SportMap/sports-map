738b93fb8e4bd47feec1f42614fdb67ec2d057d2
AntoniKania <antek.kania@wp.pl>
2023-12-09 23:13:18 +0100

    #5 Development env setup v2

--- backend/src/main/java/pl/edu/pja/sportsmap/persistence/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.pja.sportsmap.persistence.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.edu.pja.sportsmap.persistence.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- backend/src/main/java/pl/edu/pja/sportsmap/persistence/model/Review.java
+++ backend/src/main/java/pl/edu/pja/sportsmap/persistence/model/Review.java
@@ -25,5 +25,5 @@ public class Review {
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private Users user;
 }
