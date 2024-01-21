854265aeabafd31237f4d0f4e4b9ec2093851477
Kacper-Tokarzewski <103387051+Kacper-Tokarzewski@users.noreply.github.com>
2024-01-22 00:49:48 +0100

    Added register logic
    Added register logic, login works afterworks as well

--- frontend/login_register_engine.js
+++ frontend/login_register_engine.js
@@ -36,6 +36,46 @@ function login() {
     });
 }
 
+function register() {
+    const username = document.getElementById('username_reg').value;
+    const password = document.getElementById('password_reg').value;
+    const email = document.getElementById('email').value;
+    const rememberMe = document.getElementById('accept_terms').checked;
+    const requestData = {
+        username: username,
+        password: password,
+        email: email
+    };
+
+    fetch('http://localhost:8080/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+        credentials: 'include',  // Włącz przekazywanie ciasteczek w żądaniu
+    })
+    .then(response => {
+        // Sprawdź, czy odpowiedź ma status 200 OK
+        if (response.ok) {
+            console.log("Pomyślnie zarejestrowano uzytkownika");
+            const expires = new Date(Date.now() + 1 * 24 * 60 * 60 * 1000).toUTCString();
+            document.cookie = `justRegistered=true; expires=${expires}; path=/;`;
+            document.cookie = `username=${username}; expires=${expires}; path=/;`;
+            window.location.reload();
+        } else {
+            throw new Error('Błąd rejestracji');
+        }
+    })
+    .then(data => {
+        // Tutaj możesz użyć danych z odpowiedzi, jeśli są potrzebne
+        console.log('Zarejestrowano pomyślnie!');
+    })
+    .catch(error => {
+        console.error('Błąd rejestracji:', error);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     if (document.cookie.includes('justLoggedIn=')) {
         // Usuń cookie
@@ -46,5 +86,16 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 2000);
     } else {
         console.log('Użytkownik niezalogowany');
+    }
+
+    if (document.cookie.includes('justRegistered=')) {
+        // Usuń cookie
+        document.cookie = `justRegistered=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+        document.getElementById('successMessageReg').style.display = 'block';
+        setTimeout(() => {
+            document.getElementById('successMessageReg').style.display = 'none';
+        }, 2000);
+    } else {
+        console.log('Użytkownik nie rejestrowal sie');
     }
 });
