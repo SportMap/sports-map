88b0a0aaafdeac340e3b9c607c8be8be37520709
AntoniKania <antek.kania@wp.pl>
2024-01-21 22:07:32 +0100

    fix pagination fr this time

--- backend/src/main/java/pl/edu/pja/sportsmap/persistence/dao/SportComplexRepository.java
+++ backend/src/main/java/pl/edu/pja/sportsmap/persistence/dao/SportComplexRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface SportComplexRepository extends JpaRepository<SportComplex, Long> {
     List<SportComplex> findAllByStatus(SportComplexStatus status);
+    long countByStatus(SportComplexStatus status);
 }
