22e5083cffc66ec0b8951199203d69e41bee2d52
AntoniKania <antek.kania@wp.pl>
2024-01-21 12:29:44 +0100

    NOISSUE: Change date and time format

--- backend/src/main/java/pl/edu/pja/sportsmap/dto/TimeRangeDto.java
+++ backend/src/main/java/pl/edu/pja/sportsmap/dto/TimeRangeDto.java
@@ -1,5 +1,6 @@
 package pl.edu.pja.sportsmap.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,6 +9,8 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @Getter
 public class TimeRangeDto {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime openTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime closeTime;
 }
--- backend/src/main/java/pl/edu/pja/sportsmap/dto/review/GetReviewDto.java
+++ backend/src/main/java/pl/edu/pja/sportsmap/dto/review/GetReviewDto.java
@@ -1,8 +1,16 @@
 package pl.edu.pja.sportsmap.dto.review;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
 @Builder
-public record GetReviewDto(String nickname, LocalDate date, Integer rate, String content, String avatar) {
+public record GetReviewDto(
+        String nickname,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        LocalDate date,
+        Integer rate,
+        String content,
+        String avatar
+) {
 }
