ba0c76e1af70ba4f1b58ccec74e100419658416a
hepmic <51704161+hepmic@users.noreply.github.com>
2024-01-26 03:39:27 +0100

    Update 02-random-data.sql

--- backend/initdb/02-random-data.sql
+++ backend/initdb/02-random-data.sql
@@ -43,10 +43,10 @@ INSERT INTO sport_complexes (
 (1, 'Aquastacja i Sportstacja', 'Aquastacja to pełnowymiarowy basen pływacki z 6 torami, o głębokości 1,20 m do 1,80 m. Dodatkowo znajdują się dwa małe baseny o wymiarach 8x4 m, przeznaczone do nauki pływania niemowląt, dzieci i dorosłych oraz do aquaaerobiku. Strefa saun składa się z sauny suchej, łaźni parowej, laconium, misy do stóp oraz miejsc do schładzania. Sportstacja to multisportowa sala gimnastyczna o powierzchni 630 m2, przeznaczona do piłki nożnej, siatkówki, koszykówki, streetballa, badmintona oraz judo.', 'www.aquastacja.pl', 'TARTAN', 'PŁYWANIE', '54.39922', '18.57646', '1', '0', '1.jpg', '58 764 62 61', 'APPROVED'),
 (2, 'Gdański Klub Wioślarski', 'W naszej ofercie poza prowadzeniem zajęć wioślarskich dla dzieci i młodzieży mamy wynajem obiektów sportowych: hala do gry (piłka halowa, koszykówka, unihokej, siatkówka), siłownia, sala aerobiku, sala ergometrów oraz organizację obozów sportowych. Doskonale wyposażony obiekt sportowy z wyczynową sekcją wioślarską. W ofercie poza prowadzeniem zajęć wioślarskich dla dzieci i młodzieży do dyspozycji podmiotów zewnętrznych wynajmujemy obiekt sportowy: hala do gry (piłka nożna halowa, koszykówka, unihokej, siatkówka), siłownia, sala aerobiku, sala ergometrów oraz organizujemy obozy sportowe. ', 'www.gkw-drakkar.pl', 'TARTAN', 'LEKKOATLETYKA', '54.35748', '18.69177', '2', '0', '2.webp', '58 304 22 66', 'APPROVED'),
 (3, 'Gdański Klub Sportowy Gedania 1922', 'Gedania Gdańsk – najstarszy polski klub piłkarski działający w Gdańsku. Założony 15 sierpnia 1922 jako wielosekcyjny klub sportowy.', 'www.gedania1922.pl', 'TARTAN', 'LEKKOATLETYKA', '54.39702', '18.62312', '3', '0', '3.png', '517517879', 'APPROVED'),
-(4, 'Hala OLIVIA', 'Hala Sportowo-Widowiskowa Olivia znana jest nie tylko w Polsce, ale również poza jej granicami. Powstała w 1970 r.Do dzisiaj jest jedynym obiektem tego typu na Pomorzu. Podmiotem zarządzającym obiektem jest Gdański Klub Sportowy Stoczniowiec. W latach 1995 - 2000 obiekt został zmodernizowany. Obecnie jako jedyny w Polsce dysponuje dwoma zakrytymi lodowiskami i zapleczem sportowym o tak wysokim standardzie.', 'www.stoczniowiec.org.pl', 'LÓD', 'LYŻWIARSTWO', '54.40140', '18.57201', '4', '0', '4.jpg', '58 552 20 91', 'APPROVED'),
+(4, 'Hala OLIVIA', 'Hala Sportowo-Widowiskowa Olivia znana jest nie tylko w Polsce, ale również poza jej granicami. Powstała w 1970 r.Do dzisiaj jest jedynym obiektem tego typu na Pomorzu. Podmiotem zarządzającym obiektem jest Gdański Klub Sportowy Stoczniowiec. W latach 1995 - 2000 obiekt został zmodernizowany. Obecnie jako jedyny w Polsce dysponuje dwoma zakrytymi lodowiskami i zapleczem sportowym o tak wysokim standardzie.', 'www.stoczniowiec.org.pl', 'LÓD', 'LYŻWIARSTWO', '54.40140', '18.57201', '4', '1', '4.jpg', '58 552 20 91', 'APPROVED'),
 (5, 'Centrum Sportowe U-7', 'Centrum U7 Gdańsk znajduje się w historycznym, poniemieckim bunkrze z 1942 roku. Otwarcie kręgielni nastąpiło 17 stycznia 1997 roku. Jednocześnie była to pierwsza automatyczna kręgielnia w Polsce. Obecnie znajduje się tam:   8 torów bowlingowych,    10 stołów do gry w bilarda (pool),    bogato wyposażony bar,    wygodne sofy i kanapy,    rzutnik wraz ekranem o przekątnej 100 cali.', 'https://www.u7.pl/', 'DREWNIANA_PODŁOGA', 'KRĘGLE', '54.35325', '18.65241', '5', '0', '5.jpg', '58 305 55 77', 'APPROVED'),
 (6, 'Klub Wodny Żabi Kruk', 'Wypożyczalnia Kajaków i Sprzętu Wodnego dla Klientów Indywidualnych Zapraszamy do zwiedzania Gdańska z zupełnie innej perspektywy. Wypożyczalnia czynna codziennie od 10:00 do 18:00 Dopasujemy sprzęt do twoich indywidualnych potrzeb. Zaplanuj trasę wycieczki – zadzwoń 58 305 73 10 i płyniesz… Nie ma potrzeby rezerwacji pojedynczych sztuk.', 'https://kajakiempogdansku.pl/', 'ASFALT', 'LEKKOATLETYKA', '54.34239', '18.64825', '6', '0', '6.jpg', '501710010', 'APPROVED'),
-(7, 'Obiekt Sportowy Zielonogórska', 'Ogólnodostępny obiekt sportowy znajduje się na gdańskim Chełmie przy ul. Zielonogórskiej 4 (dojazd autobusami linii 108 lub 118 do przystanku n/ż ul. Stoczniowców). Do dyspozycji mieszkańców są tutaj korty tenisowe, dwa wymiarowe boiska piłkarskie, na których są rozgrywane m.in. mecze ligowe klasy „B” oraz mini skatepark.', 'https://sportgdansk.pl/obiekty/obiekt-sportowy-zielonogorska/', 'TRAWA', 'PIŁKA_NOŻNA', '54.34293', '18.63472', '7', '0', '7.webp', '601653968', 'APPROVED'),
+(7, 'Obiekt Sportowy Zielonogórska', 'Ogólnodostępny obiekt sportowy znajduje się na gdańskim Chełmie przy ul. Zielonogórskiej 4 (dojazd autobusami linii 108 lub 118 do przystanku n/ż ul. Stoczniowców). Do dyspozycji mieszkańców są tutaj korty tenisowe, dwa wymiarowe boiska piłkarskie, na których są rozgrywane m.in. mecze ligowe klasy „B” oraz mini skatepark.', 'https://sportgdansk.pl/obiekty/obiekt-sportowy-zielonogorska/', 'TRAWA', 'PIŁKA_NOŻNA', '54.34293', '18.63472', '7', '1', '7.webp', '601653968', 'APPROVED'),
 (8, 'Kort tenisowy', 'brak', 'brak', 'TWARDY_KORT', 'TENIS', '54.35923', '18.62800', '8', '1', '8.jpg', '-', 'APPROVED'),
 (9, 'CrossFit Stocznia', 'Nasz Box ma charakter szkoły treningowej, z klasami według poziomu zaawansowania.', 'www.crossfitstocznia.com', 'DREWNIANA_PODŁOGA', 'SPORTY_SIŁOWE', '54.36180', '18.65440', '9', '0', '9.jpeg', '732884530', 'APPROVED'),
 (10, 'Strzelnica LOK Gdańsk', 'Nasz obiekt to doskonałe miejsce dla każdego, kto szuka oryginalnego sposobu na spędzenie wolnego czasu i wyśmienitej rozrywki. Możesz przekona jak strzela się z prawdziwej broni. Huk wystrzałów, zapach prochu i adrenalina towarzysząca strzelaniu przekłada się na satysfakcję wynikającą z kolejnych trafień. Dzięki wykwalifikowanej kadrze instruktorskiej poznasz zasady bezpieczeństwa, celowania i celnego oddania strzału. Możesz przyjść sam lub w grupie przyjaciół i znajomych', 'https://lokgdansk.pl/', 'DREWNIANA_PODŁOGA', 'LEKKOATLETYKA', '54.36266', '18.63517', '10', '0', '10.jpg', '785882825', 'APPROVED'),
@@ -66,17 +66,17 @@ INSERT INTO sport_complexes (
 (13, 'Centrum Sportu Akademickiego Politechniki Gdańskiej', 'Ośrodek sportowy z pełnowymiarowym boiskiem piłkarskim z certyfikatem FIFA, halą sportową, boiskiem do siatkówki, plenerowymi kortami tenisowymi i halą tenisową, salą aerobiku, siłownią, wioślarnią, salą judo, sauną, a także oferujący zajęcia fitness i sportowe dla dzieci i dorosłych.', 'http://csa.pg.edu.pl/', 'DREWNIANA_PODŁOGA', 'LEKKOATLETYKA', '54.36936', '18.63143', '13', '0', '13.jpg', '583472500', 'APPROVED'),
 (14, 'Boisko sportowe (Szkolne)', 'brak', 'http://sp50.edu.gdansk.pl/', 'TARTAN', 'PIŁKA_NOŻNA', '54.35291', '18.65599', '14', '0', '14.jpg', '58 301 18 48', 'APPROVED'),
 (15, 'Strzelnica Sportowa Klub Strzelających Inaczej KSI', 'Klub Strzelających Inaczej „KSI”, powstał z inicjatywy obywateli szukających alternatyw w strzelectwie oraz możliwości rozwoju w zakresie strzelania sytuacyjnego', 'https://www.strzelamyinaczej.pl/', 'ASFALT', 'LEKKOATLETYKA', '54.34748', '18.67131', '15', '0', '15.webp', '530386445', 'APPROVED'),
-(16, 'Stadion im. Zbigniewa Podleckiego', 'Początki żużla w Gdańsku sięgają pierwszych lat powojennych. Pierwszym obiektem z prawdziwego zdarzenia był stadion przy ul. Marynarki Polskiej. Obiekt, który wówczas służył również piłkarzom, teraz jest tylko wykorzystywany przez futbolistów Polonii Gdańsk. ', 'https://www.wybrzezegdansk.pl/stadion', 'TRAWA', 'PIŁKA_NOŻNA', '54.34615', '18.67157', '16', '0', '16.jpg', '58 346 30 27', 'APPROVED'),
-(17, 'KS Polonia', 'Polski klub piłkarski z siedzibą w Gdańsku, powstały 27 września 1945. Obecnie występuje w rozgrywkach Klasy okręgowej w Grupie Gdańskiej I', 'http://www.90minut.pl/skarb.php?id_klub=309&id_sezon=', 'TRAWA', 'PIŁKA_NOŻNA', '54.37396', '18.63485', '17', '0', '17.jpg', '795003803', 'APPROVED'),
+(16, 'Stadion im. Zbigniewa Podleckiego', 'Początki żużla w Gdańsku sięgają pierwszych lat powojennych. Pierwszym obiektem z prawdziwego zdarzenia był stadion przy ul. Marynarki Polskiej. Obiekt, który wówczas służył również piłkarzom, teraz jest tylko wykorzystywany przez futbolistów Polonii Gdańsk. ', 'https://www.wybrzezegdansk.pl/stadion', 'TRAWA', 'PIŁKA_NOŻNA', '54.34615', '18.67157', '16', '1', '16.jpg', '58 346 30 27', 'APPROVED'),
+(17, 'KS Polonia', 'Polski klub piłkarski z siedzibą w Gdańsku, powstały 27 września 1945. Obecnie występuje w rozgrywkach Klasy okręgowej w Grupie Gdańskiej I', 'http://www.90minut.pl/skarb.php?id_klub=309&id_sezon=', 'TRAWA', 'PIŁKA_NOŻNA', '54.37396', '18.63485', '17', '1', '17.jpg', '795003803', 'APPROVED'),
 (18, 'FitYard', 'FitYard, kameralna siłownia dla ludzi lubiących przyjemną atmosferę i spokój. Na wyposażeniu profesjonalny sprzęt z pomocą którego zrobisz co chcesz, a nawet więcej.', 'https://fityard.pl/', 'DREWNIANA_PODŁOGA', 'SPORTY_SIŁOWE', '54.37211', '18.63409', '18', '0', '18.jpg', '48506602995', 'APPROVED'),
-(19, 'Pomorskie Centrum Hokejowe', 'Profesjonalne treningi hokejowe oraz ostrzenie łyżew optymalizowane pod wagę sportowca. Hala z lodowiskiem syntetycznym o wymiarach 8x24m pozwalającym rozgrywać mecze 3x3 plus bramkarze.', 'https://centrumhokejowe.pl/', 'LÓD', 'LYŻWIARSTWO', '54.37574', '18.63453', '19', '0', '19.jpg', '48504796999', 'APPROVED'),
-(20, 'Siłownia zewnętrzna', 'Siłownia zewnętrzna', 'brak', 'TRAWA', 'SPORTY_SIŁOWE', '54.34660', '18.64269', '20', '0', '20.jpg', '-', 'APPROVED'),
+(19, 'Pomorskie Centrum Hokejowe', 'Profesjonalne treningi hokejowe oraz ostrzenie łyżew optymalizowane pod wagę sportowca. Hala z lodowiskiem syntetycznym o wymiarach 8x24m pozwalającym rozgrywać mecze 3x3 plus bramkarze.', 'https://centrumhokejowe.pl/', 'LÓD', 'LYŻWIARSTWO', '54.37574', '18.63453', '19', '1', '19.jpg', '48504796999', 'APPROVED'),
+(20, 'Siłownia zewnętrzna', 'Siłownia zewnętrzna', 'brak', 'TRAWA', 'SPORTY_SIŁOWE', '54.34660', '18.64269', '20', '1', '20.jpg', '-', 'APPROVED'),
 (21, 'Boiska do koszykówki', 'Boiska do koszykówki', 'brak', 'SZTUCZNA_TRAWA', 'KOSZYKÓWKA', '54.34567', '18.64267', '21', '1', '21.jpg', '-', 'APPROVED'),
 (22, 'Pływalnia Chełm', 'Pływalnia CHEŁM przy ul. Chałubińskiego na gdańskim Chełmie jest ważnym ośrodkiem rekreacyjnym na mapie miasta. Już od 18 lat służy gdańszczanom, jako kompleks basenów o szerokim profilu działalności.Jest czynna przez 7 dni w tygodniu od godzin wczesnorannych, aż do godziny 22. Cieszy się bardzo dużą popularnością użytkowników w różnym wieku, ma opinię jednej z najlepszych w Gdańsku. Korzystają z niej zarówno klienci indywidualni, jak też zorganizowane grupy. W ciągu dnia do użytkowników pływalni należą szkoły miasta Gdańska. Dzieci i młodzież pod okiem instruktorów i nauczycieli oswajają się z wodą oraz doskonalą umiejętności pływackie. Dodatkowo, podczas ferii zimowych, GOS zaprasza młodych mieszkańców Gdańska na zajęcia, za które uczestnicy płacą zaledwie jedną złotówkę.', 'https://plywalniegdansk.pl/plywalnia-chelm/', 'TARTAN', 'PŁYWANIE', '54.33968', '18.62087', '22', '0', '22.jpg', '58 320 74 31', 'APPROVED'),
 (23, 'Forfit', 'Forfit to obiekt, w którym możesz skorzystać z podstawowych ćwiczeń dostępnych w każdej siłowni, czyli zajęcia siłowe i fitness, ale żeby się wyróżnić wprowadziliśmy do oferty zajęcia z CrossFitu.', 'https://www.trojmiasto.pl/Forfit-o61541.html', 'TARTAN', 'SPORTY_SIŁOWE', '54.33986', '18.62118', '23', '0', '23.jpeg', '513601272', 'APPROVED'),
 (24, 'Boiska sportowe', 'Kompleksu boisk przy Zespół Szkół Ogólnokształcących nr VII w Gdańsku', 'https://zso7.edu.gdansk.pl/pl', 'TARTAN', 'LEKKOATLETYKA', '54.33925', '18.62090', '24', '1', '24.jpg', '58 302 85 20', 'APPROVED'),
 (25, 'DSTeam Strzelnica', 'Jeśli interesuje Cię broń palna, lubisz adrenalinę i chciałbyś spróbować w życiu czegoś naprawdę ekscytującego, jesteś we właściwym miejscu! Nie potrzebujesz patentów, pozwoleń czy dopuszczeń do posiadania broni palnej!', 'https://dsteam.pl/', 'ASFALT', 'LEKKOATLETYKA', '54.33989', '18.61925', '25', '0', '25.jpeg', '665566997', 'APPROVED'),
-(26, 'Argonaut Fitness', 'Szkoła Argonaut Gdańsk – Fitness | Pływanie | Nurkowanie | Ratownictwo', 'https://argonaut.gda.pl/gdansku/', 'TARTAN', 'PŁYWANIE', '54.34182', '18.62194', '26', '0', '26.jpg', '510178819', 'APPROVED'),
+(26, 'Argonaut Fitness', 'Szkoła Argonaut Gdańsk – Fitness | Pływanie | Nurkowanie | Ratownictwo', 'https://argonaut.gda.pl/gdansku/', 'TARTAN', 'PŁYWANIE', '54.34182', '18.62194', '26', '1', '26.jpg', '510178819', 'APPROVED'),
 (27, 'Lechia Tennis Club', 'Lechia Tennis Club to obiekt  sportowy położony w zalesionej i zacisznej części Gdańska, jednak świetnie skomunikowany z centrum.  Do dyspozycji klubowiczów są cztery korty odkryte z nawierzchnią ceglaną. W sezonie zimowym zaś klubowicze mogą korzystać ze świeżo wyremontowanej hali z kortami o nawierzchni dywanowej.', 'http://lechiatennisclub.pl/', 'TWARDY_KORT', 'TENIS', '54.36650', '18.618921', '27', '0', '27.webp', '660 57 00 57', 'APPROVED'),
 (28, 'Premium Fitness & Gym', 'PREMIUM FITNESS & GYM to miejsce stworzone przez pasjonatów sportu i skierowane do wszystkich jego miłośników. Profesjonalnie wyposażona siłownia, bogata oferta zajęć fitness, wykwalifikowana kadra, a do tego świetna atmosfera. Stawiamy sobie za cel, abyś to Ty razem z nami zrealizował wszystkie swoje cele!', 'https://premiumgym.pl/', 'DREWNIANA_PODŁOGA', 'SPORTY_SIŁOWE', '54.33648', '18.62045', '28', '0', '28.jpg', '504413414', 'APPROVED'),
 (29, 'Boisko', 'Boisko', 'brak', 'TARTAN', 'LEKKOATLETYKA', '54.33685', '18.62205', '29', '1', '29.jpg', '-', 'APPROVED'),
@@ -85,7 +85,7 @@ INSERT INTO sport_complexes (
 (32, 'Stół do gry w tenisa stołowego', 'Stół do gry w tenisa stołowego', 'brak', 'PIASEK', 'TENIS_STOŁOWY', '54.33579', '18.62491', '32', '1', '32.jpg', '-', 'APPROVED'),
 (33, 'Bieżnia przy Szkole Podstawowej nr 47', 'Bieżnia i obiekt sportowy przy Szkole Podstawowej nr 47', 'https://sp47.edu.gdansk.pl/pl', 'TARTAN', 'LEKKOATLETYKA', '54.34181', '18.63176', '33', '1', '33.jpg', '58 302 03 28', 'APPROVED'),
 (34, 'Skatepark im. Św. Nikodema małego', 'Skatepark im. Św. Nikodema małego', 'brak', 'ASFALT', 'LEKKOATLETYKA', '54.34346', '18.63448', '34', '1', '34.jpg', '-', 'APPROVED'),
-(35, 'Gdański Ośrodek Sportu', 'Boisko do piłki nożnej', 'https://sportgdansk.pl/', 'TRAWA', 'PIŁKA_NOŻNA', '54.34254', '18.63597', '35', '1', '35.jpg', '-', 'APPROVED'),
+(35, 'Gdański Ośrodek Sportu', 'Boisko do piłki nożnej', 'https://sportgdansk.pl/', 'TRAWA', 'PIŁKA_NOŻNA', '54.34254', '18.63597', '35', '1', '35.jpg', '-', 'APPROVED')
 ;
 
 INSERT INTO events (name, description, sport_complex_id, start_time, end_time, photo, user_id)
@@ -297,6 +297,60 @@ VALUES
     (2, '13:00', '21:00', 18),  -- Wednesday
     (3, '13:00', '21:00', 18),  -- Thursday
     (4, '13:00', '21:00', 18);  -- Friday
+
+-- Sport Complex 22
+INSERT INTO opening_hours (day_of_week, opening_time, closing_time, sport_complex_id)
+VALUES
+    (0, '16:00', '22:00', 22),
+    (1, '16:00', '22:00', 22),
+    (2, '16:00', '22:00', 22),
+    (3, '16:00', '22:00', 22),
+    (4, '16:00', '22:00', 22),
+    (5, '07:00', '21:00', 22),
+    (6, '07:00', '21:00', 22);
+
+-- Sport Complex 23
+INSERT INTO opening_hours (day_of_week, opening_time, closing_time, sport_complex_id)
+VALUES
+    (0, '08:00', '22:00', 23),
+    (1, '08:00', '22:00', 23),
+    (2, '08:00', '22:00', 23),
+    (3, '08:00', '22:00', 23),
+    (4, '08:00', '22:00', 23),
+    (5, '10:00', '18:00', 23),
+    (6, '10:00', '16:00', 23);
+
+-- Sport Complex 25
+INSERT INTO opening_hours (day_of_week, opening_time, closing_time, sport_complex_id)
+VALUES
+    (0, '13:00', '21:00', 25),
+    (2, '13:00', '21:00', 25),
+    (3, '13:00', '21:00', 25),
+    (4, '13:00', '21:00', 25),
+    (5, '12:00', '22:00', 25),
+    (6, '12:00', '20:00', 25);
+
+-- Sport Complex 27
+INSERT INTO opening_hours (day_of_week, opening_time, closing_time, sport_complex_id)
+VALUES
+    (0, '08:00', '22:00', 27),
+    (1, '08:00', '22:00', 27),
+    (2, '08:00', '22:00', 27),
+    (3, '08:00', '22:00', 27),
+    (4, '08:00', '22:00', 27),
+    (5, '08:00', '22:00', 27),
+    (6, '08:00', '22:00', 27);
+
+-- Sport Complex 28
+INSERT INTO opening_hours (day_of_week, opening_time, closing_time, sport_complex_id)
+VALUES
+    (0, '06:00', '00:00', 28),
+    (1, '06:00', '00:00', 28),
+    (2, '06:00', '00:00', 28),
+    (3, '06:00', '00:00', 28),
+    (4, '06:00', '00:00', 28),
+    (5, '06:00', '00:00', 28),
+    (6, '06:00', '00:00', 28);
 
 
 INSERT INTO users (username, email, password, authority, avatar)
